44037e98f7e662ea2988a76a8272970ad063ae37
Elizaveta Zavodovskaya <54911483+zavad4@users.noreply.github.com>
2021-01-08 22:41:21 +0200

    checking errors.

--- Bot/modules/db.module.js
+++ Bot/modules/db.module.js
@@ -4,5 +4,9 @@ const UIDGenerator = require('uid-generator');
 const pool = require('../../DB/pool');
 
 process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0;
+process.on('unhandledRejection', error => {
+  console.log(error);
+  pool.end();
+});
 
 const uidgen = new UIDGenerator();
