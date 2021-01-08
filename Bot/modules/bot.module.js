5f1799264897cf11d8eded4d0db97f96995f33df
zemark-prog <54912616+zemark-prog@users.noreply.github.com>
2021-01-08 18:49:42 +0200

    Constants generated and added

---
+++ Bot/modules/bot.module.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const axios = require('axios');
+const UIDGenerator = require('uid-generator');
+const db = require('./db.module');
+const { statusChanger, courses } = require('./status.module');
+const createExcel = require('./excel.module');
+
+const uidgen = new UIDGenerator(); // token generator
+const statuses = statusChanger();
+
+
