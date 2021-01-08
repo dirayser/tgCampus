fec7edc2dc6b0d45578cde67980a8bf6eda26048
Zegelman Mark <54912616+zemark-prog@users.noreply.github.com>
2021-01-08 23:59:06 +0200

    Insert Course finished

--- Bot/modules/db.module.js
+++ Bot/modules/db.module.js
@@ -56,8 +56,26 @@ function insertCourse(ctx, courses, userID, username) { // adds course for teach
   const query = `INSERT INTO Courses VALUES
   ('${currCourse.courseID}', '${currCourse.courseName}', ${userID}, 
     ${currCourse.labNumb}, ${currCourse.testNumb}, ${+currCourse.additional})`;
-  const promise = new Promise((resolve, reject) =>    
-  }
+  const promise = new Promise((resolve, reject) => {
+    (async () => {
+      const client = await pool.connect();
+      try {
+        await client.query(query);
+        await client.release();
+        ctx.reply('Курс добавлен');
+        const isRegistred = await isTeacherRegistred(userID);
+        if (!isRegistred) {
+          await insertTeacher(ctx, userID, username);
+        }
+        resolve();
+      } catch (e) {
+        await client.release();
+        console.log(e);
+        ctx.reply('Курс не добавлен');
+        reject();
+      }
+    })();
+  });
   return promise;
 }
 
