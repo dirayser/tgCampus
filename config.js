6cadc42e574adbed0b4c4cccf9d6e37c24f3f0a9
Dmytro <dirayser@gmail.com>
2021-01-08 17:07:08 +0200

    add config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  user: process.env.dbUser,
+  host: process.env.dbHost,
+  database: process.env.dbName,
+  password: process.env.dbPswd,
+  port: process.env.port,
+  botToken: process.env.botToken,
+  ssl: true,
+}
