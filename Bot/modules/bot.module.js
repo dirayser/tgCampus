44076289f57428068c1f3e0b6f60e054774cf6c2
Elizaveta Zavodovskaya <54911483+zavad4@users.noreply.github.com>
2021-01-08 19:01:38 +0200

    onCourseNameGet function added

--- Bot/modules/bot.module.js
+++ Bot/modules/bot.module.js
@@ -9,4 +9,8 @@ const createExcel = require('./excel.module');
 const uidgen = new UIDGenerator(); // token generator
 const statuses = statusChanger();
 
-
+function onCourseNameGet(ctx, text, userID) { // on courseName waited
+  courses[userID].courseName = text;
+  statuses.set(userID, 'wait:labNumb');
+  ctx.reply('Количество лабораторных работ в курсе:');
+}
