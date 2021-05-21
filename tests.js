099ea70aa41b4833a1037600d82cbbbb58b43534
Mark Zegelman <zemark2012@gmail.com>
2021-05-21 13:20:32 +0300

    tests added

--- tests.js
+++ tests.js
@@ -14,7 +14,13 @@ console.assert(statuses.get('id002') === ''); //2
 statuses.set('id002', 10);
 console.assert(statuses.get('id002') === 10); //3
 
+statuses.set(2, 5);
+console.assert(statuses.get(2) === 5); //4
+
 statuses.unset('id001');
-console.assert(statuses.get('id001') === undefined); //4
+console.assert(statuses.get('id001') === undefined); //5
+
+statuses.unset('id001');
+console.assert(statuses.get('id001') === undefined); //6
 
 console.assert(new Telegraf().telegram.options.apiMode === 'bot');
