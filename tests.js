0a71db2121f2ad180206decc58a6397ea2c0e997
zavad4 <lizavodovska@gmail.com>
2021-05-21 12:42:51 +0300

    add some tests

--- tests.js
+++ tests.js
@@ -1,5 +1,10 @@
 'use strict';
 
 const { Telegraf } = require('telegraf');
+const { statusChanger } = require('./Bot/modules/status.module');
+
+const statuses = statusChanger();
+statuses.set('id001', 'expected');
+console.assert('expected' === statuses.get('id001')); //1
 
 console.assert(new Telegraf().telegram.options.apiMode === 'bot');
