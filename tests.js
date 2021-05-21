fe6ba6a5f437236d43de63bc072dee2abb12b5c0
Mark Zegelman <zemark2012@gmail.com>
2021-05-21 13:08:00 +0300

    test added

--- tests.js
+++ tests.js
@@ -6,5 +6,7 @@ const { statusChanger } = require('./Bot/modules/status.module');
 const statuses = statusChanger();
 statuses.set('id001', 'expected');
 console.assert('expected' === statuses.get('id001')); //1
+statuses.unset('id001');
+console.assert(undefined === statuses.get('id001')); //2
 
 console.assert(new Telegraf().telegram.options.apiMode === 'bot');
