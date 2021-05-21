7e3817f4c27ad0fc5e4793f498d2de324c5d6b91
zavad4 <lizavodovska@gmail.com>
2021-05-21 22:34:08 +0300

    add serialize calling

--- Bot/modules/bot.module.js
+++ Bot/modules/bot.module.js
@@ -73,7 +73,8 @@ function onTestNumGet(ctx, text, userID) { // on testsNumb waited
 function onAdditGet(ctx, text, userID) { // on additional waited
   courses[userID].additional = text.toLowerCase() === 'y';
   statuses.set(userID, 'wait:check');
-  ctx.reply('Всё верно? (y/n)\n' + JSON.stringify(courses[userID]));
+  const message = serialize(...Object.values(courses[userID]));
+  ctx.reply(message);
 }
 
 function serialize (courseID, courseName, labNumb, testNumb, additional) {
