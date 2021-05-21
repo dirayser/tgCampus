75058881f4f556ce1465e172a976fd0152912edb
zavad4 <lizavodovska@gmail.com>
2021-05-21 13:16:37 +0300

    add some tests

--- tests.js
+++ tests.js
@@ -4,9 +4,17 @@ const { Telegraf } = require('telegraf');
 const { statusChanger } = require('./Bot/modules/status.module');
 
 const statuses = statusChanger();
+
 statuses.set('id001', 'expected');
-console.assert('expected' === statuses.get('id001')); //1
+console.assert(statuses.get('id001') === 'expected'); //1
+
+statuses.set('id002', '');
+console.assert(statuses.get('id002') === ''); //2
+
+statuses.set('id002', 10);
+console.assert(statuses.get('id002') === 10); //3
+
 statuses.unset('id001');
-console.assert(undefined === statuses.get('id001')); //2
+console.assert(statuses.get('id001') === undefined); //4
 
 console.assert(new Telegraf().telegram.options.apiMode === 'bot');
